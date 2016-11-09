80ff605f6086762fb47e54f5b92fbb8efbca2db3
Anders Palm <ang.palm@hotmail.com>
2016-11-09 14:34:27 -0500

    added insertion sort algorithm

--- SortingAlogrithms/src/SeectionSort.java
+++ SortingAlogrithms/src/SeectionSort.java
@@ -7,6 +7,9 @@ public class SeectionSort {
 
         int[] input = {10,34,2,56,7,67,88,42,2,13,432,21,123,4432,1223,4432,12213,212131,32132,13,21,321,32,13,21,21,3,213,21,32,324,
                 14,32,431,24,321,4,321,4321,4,321,4,321,4,321,4,321,34,2,56,7,67,88,42,2,13,432,21,123};
+
+
+        System.out.println("Selection");
         int[] seletionOutput = doSelectionSort(input);
         for(int i = 0; i < seletionOutput.length; i ++){
             if (i < input.length - 1) {
@@ -19,12 +22,29 @@ public class SeectionSort {
 
         System.out.println("-----------------------------------------------");
 
+        System.out.println("bubble");
         int[] bubbleOutput = bubbleSort(input);
-        String total = "";
-        for (int i : bubbleOutput){
-            total += " " + i;
+        for(int i = 0; i < bubbleOutput.length; i ++){
+            if (i < input.length - 1) {
+                System.out.print(bubbleOutput[i] + ", ");
+            }
+            else {
+                System.out.print(bubbleOutput[i] + ".");
+            }
         }
-        System.out.println(total);
+
+        System.out.println("-----------------------------------------------");
+
+        System.out.println("insertion");
+        int[] insertionOutput = insertionSort(input);
+        for(int i = 0; i < insertionOutput.length; i ++){
+            if (i < input.length - 1) {
+                System.out.print(insertionOutput[i] + ", ");
+            }
+            else {
+                System.out.print(insertionOutput[i] + ".");
+            }
+        }
     }
 
     public static int[] doSelectionSort(int[] arr){
@@ -58,6 +78,20 @@ public class SeectionSort {
 
         }
 
+        return arr;
+    }
+
+    public static int[] insertionSort(int[] arr){
+     int temp = 0;
+        for(int i = 1; i < arr.length; i ++){
+            for(int j = i; j > 0; j --) {
+                if(arr[j] < arr[j -1]){
+                    temp = arr[j];
+                    arr[j] = arr[j - 1];
+                    arr[j - 1] = temp;
+                }
+            }
+        }
         return arr;
     }
 
