fee04600bf27b8aa973598dba2c016fc69befc1e
Anders Palm <ang.palm@hotmail.com>
2016-11-09 15:34:47 -0500

    added quickSort algorithm

--- SortingAlogrithms/src/SeectionSort.java
+++ SortingAlogrithms/src/SeectionSort.java
@@ -5,8 +5,7 @@ public class SeectionSort {
 
     public static void main(String a[]){
 
-        int[] input = {10,34,2,56,7,67,88,42,2,13,432,21,123,4432,1223,4432,12213,212131,32132,13,21,321,32,13,21,21,3,213,21,32,324,
-                14,32,431,24,321,4,321,4321,4,321,4,321,4,321,4,321,34,2,56,7,67,88,42,2,13,432,21,123};
+        int[] input = {10,9,8,7,6,5,4,3,2,1,1,2,3,4,5,6,7,8,9,10};
 
 
         System.out.println("Selection");
@@ -21,7 +20,6 @@ public class SeectionSort {
         }
 
         System.out.println("-----------------------------------------------");
-
         System.out.println("bubble");
         int[] bubbleOutput = bubbleSort(input);
         for(int i = 0; i < bubbleOutput.length; i ++){
@@ -34,7 +32,6 @@ public class SeectionSort {
         }
 
         System.out.println("-----------------------------------------------");
-
         System.out.println("insertion");
         int[] insertionOutput = insertionSort(input);
         for(int i = 0; i < insertionOutput.length; i ++){
@@ -43,6 +40,18 @@ public class SeectionSort {
             }
             else {
                 System.out.print(insertionOutput[i] + ".");
+            }
+        }
+
+        System.out.println("-----------------------------------------------");
+        System.out.println("quickSort");
+        int[] quickSort = quickSort(input, 0, input.length - 1);
+        for(int i = 0; i < quickSort.length; i ++){
+            if (i < input.length - 1) {
+                System.out.print(quickSort[i] + ", ");
+            }
+            else {
+                System.out.print(quickSort[i] + ".");
             }
         }
     }
@@ -93,6 +102,41 @@ public class SeectionSort {
             }
         }
         return arr;
+    }
+
+//    public static int[] quickSort(int[] arr){
+//
+//        int pivot =
+//
+//    }
+
+
+    public static int[] quickSort(int[] array, int beg, int end) {
+        int i = beg;
+        int j = end;
+        int pivot = 0;
+        if (end - beg >= 1){
+                pivot = array[end];
+
+            while (j > i) {
+                while (array[i] <= pivot && i <= end && j > i) {i++;} // count up until a value is smaller than the pivot.
+                while (array[j] > pivot && j >= beg && j >= i) {j--;} // count down until a value is larger than the pivot.
+                if (j > i) {
+                    swapIndexes(array, i, j);
+                    // as long as the new end index is larger than the new start swap the values
+                }
+            }
+            quickSort(array, beg, j - 1); // restart the process with the new left-hand range
+            quickSort(array, j + 1, end); // restart with the new right-hand range
+        }
+        return array; // once there are no unsorted items return the finished array
+    }
+
+    public static void swapIndexes(int[] array, int i, int j){
+//      below is a staple index value swap
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
     }
 
 }
